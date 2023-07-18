9f42595b27874e6d6038d9aa62f881eb5dfbb907
Bruno Brigido Vilanova <brunobrigidovilanova@gmail.com>
2023-07-18 14:12:08 -0300

    estlizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/brunobrigidovilanova.png" />
-    <Titulo fontSize={20}>Bruno Brigido Vilanova</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Bruno Brigido Vilanova</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        brunobrigidovilanova
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
